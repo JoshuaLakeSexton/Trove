76c785f858eb20fb3783ba9f60c39efd7894b285
JoshuaLakeSexton <sextoninventive@gmail.com>
2024-09-01 08:31:02 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Example of a simple smooth scroll for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Additional JS functionalities can be added here
+});
